3210e9bcf8725d7910ace32380685d5ea135c16d
tzikos <dimichatzakis@gmail.com>
2024-07-14 19:20:04 +0300

    added comments and separated visualizations for captum

--- xplainLeads.py
+++ xplainLeads.py
@@ -8,50 +8,72 @@ import torch.nn.functional as F
 import os
 from utility import seedEverything
 
+# Seed everything for os parsing of same files
 seedEverything(42)
 
+# Globals - to be changed per case
+# Path of fold to consider
 PATH = "/home/tzikos/Desktop/Data/Berts final/pre/fold5"
-BATCH = 16
+# Batch size to take - handle per model for OOM issues
+BATCH = 20
 NUM_CLASSES = 2
 DICT = {0:"AVNRT", 1:"AVRT"}
-WINDOW_SIZE = 501
+# Show class diffs (Only for Binary Data)
+SHOW_DIFF = True
+# Load model and weights
 model = ECGCNNClassifier(numClasses=NUM_CLASSES)
 model.load_state_dict(torch.load('/home/tzikos/Desktop/weights/Models/ECGCNNClassifier_fold5_pre_B64_L1e-05_01-07-24-09-27.pth'))
+# Load Integrated Gradients object from Captum
 ig = IntegratedGradients(model)
-
+# Initialize lists to save 
+# files that will be loaded...
 allClassFiles = []
+#... as well as captums differences for each class
 diff_of_sums = []
 
+# Loop through the classes
 for i in range(NUM_CLASSES):
+    # Get all files
     allFiles = os.listdir(PATH)
     allFiles = [os.path.join(PATH, file) for file in allFiles]
+    # Filter out missing leads and irrelevant classes    
     classFiles = [file for file in allFiles if ("missing" not in file) and (DICT[i] in file)]
     allClassFiles.append(classFiles)
-#print(len(allClassFiles))
+# np array to hold the ECG data
 class_signals = np.zeros((NUM_CLASSES, BATCH, 12, 5000))
+# Initialize plotly figure to write per lead, per class results
 fig = go.Figure()
+
+# Loop through classes
 for i in range(NUM_CLASSES):
+    # Take only the first BATCH files
     allClassFiles[i] = allClassFiles[i][:BATCH]
+    # Load the ECG data
     for file in allClassFiles[i]:
         class_signals[i, :, :, :] = np.load(file).T 
-    #print(len(allClassFiles[i]))
+    # Define target class
     target_class = i
-    lead_attributions = []
+    # Initialize lists to hold the attribution sums
     lead_attr_sums = []
-    # Create a single subplot
+    # Loop through leads
     for lead in range(12):
+        # Initialize tensor to hold the lead data
         lead_signal = np.zeros((BATCH, 12, 5000))
+        # Ascribe values
         lead_signal[:, lead, :] = class_signals[i, :, lead, :]
         print(f"Processing class {i+1}, lead {lead+1}")
         try:
-            lead_signal_tensor = torch.from_numpy(lead_signal).float()  # Ensure tensor is float type
+            # Ensure tensor is tensor and float type
+            lead_signal_tensor = torch.from_numpy(lead_signal).float()
+            # Get the attributions and store them
             attribution = ig.attribute(lead_signal_tensor, target=target_class)
             print("no error in attributions")
-            lead_attributions.append(attribution)
             lead_attr_sums.append(attribution[:,lead,:].sum())
         except Exception as e:
             print(f"Error in attributions for class {i}, lead {lead}: {e}")
+    # Transform them to numpy to be visualy callable
     lead_attr_sums = [x.numpy() for x in lead_attr_sums]
+    # Store the sums for each lead to differentiate between classes
     diff_of_sums.append(lead_attr_sums)
     # Add bar chart for Class 1
     fig.add_trace(go.Bar(
@@ -59,14 +81,17 @@ for i in range(NUM_CLASSES):
         y=lead_attr_sums,
         name=f"Class {DICT[i]}"
     ))
-diff_list = [diff_of_sums[0][i]-diff_of_sums[1][i] for i in range(len(diff_of_sums[0]))]
 
-# Add bar chart for Differences
-fig.add_trace(go.Bar(
-    x=list(range(1, len(diff_list) + 1)),
-    y=diff_list,
-    name="Differences"
-))
+if SHOW_DIFF:
+    # Create differentiations
+    diff_list = [diff_of_sums[0][i]-diff_of_sums[1][i] for i in range(len(diff_of_sums[0]))]
+
+    # Add bar chart for Differences
+    fig.add_trace(go.Bar(
+        x=list(range(1, len(diff_list) + 1)),
+        y=diff_list,
+        name="Differences"
+    ))
 
 # Update layout with the provided axis settings
 fig.update_layout(
@@ -88,4 +113,5 @@ fig.update_layout(
 
 # Show figure
 fig.show()
+# Save figure
 fig.write_html(f"/home/tzikos/xplainability/histogram_binary_AVNRT_AVRT_pre_class_diffs.html")
--- xplainTS.py
+++ xplainTS.py
@@ -6,67 +6,92 @@ from models import ECGCNNClassifier
 import numpy as np
 import torch.nn.functional as F
 import os 
+from utility import seedEverything
 
+# Seed everything for os parsing of same files
+seedEverything(42)
+
+# Path of fold data wanted
 PATH = "/home/tzikos/Desktop/Data/Berts final/pre/fold5"
-BATCH = 16
+# Batch size - change per model to avoid OOM issues
+BATCH = 20
+# Number in Batch to take
+ELEMENT = 0
 NUM_CLASSES = 2
 DICT = {0:"AVNRT", 1:"AVRT"}
+# Window size for averaging filter
 WINDOW_SIZE = 501
+# Load model and weights
 model = ECGCNNClassifier(numClasses=NUM_CLASSES)
 model.load_state_dict(torch.load('/home/tzikos/Desktop/weights/Models/ECGCNNClassifier_fold5_pre_B64_L1e-05_01-07-24-09-27.pth'))
+# Load Integrated Gradients object from Captum
 ig = IntegratedGradients(model)
 
+# Initialize lists to save 
+# class files
 allClassFiles = []
-diff_of_sums = []
 
+# Loop through the classes
 for i in range(NUM_CLASSES):
+    # Get all files
     allFiles = os.listdir(PATH)
     allFiles = [os.path.join(PATH, file) for file in allFiles]
+    # Filter out missing leads and irrelevant classes
     classFiles = [file for file in allFiles if ("missing" not in file) and (DICT[i] in file)]
     allClassFiles.append(classFiles)
-#print(len(allClassFiles))
 class_signals = np.zeros((NUM_CLASSES, BATCH, 12, 5000))
+# Loop through classes
 for i in range(NUM_CLASSES):
+    # Take only the first BATCH files
     allClassFiles[i] = allClassFiles[i][:BATCH]
     for file in allClassFiles[i]:
         class_signals[i, :, :, :] = np.load(file).T 
-    #print(len(allClassFiles[i]))
+    # Ascribe the correct class for Captum
     target_class = i
+    # Initialize lists to hold the attributions
     lead_attributions = []
-    lead_attr_sums = []
     for lead in range(12):
+        # Initialize tensor to hold the lead data
         lead_signal = np.zeros((BATCH, 12, 5000))
         lead_signal[:, lead, :] = class_signals[i, :, lead, :]
-        print(f"Processing class {i}, lead {lead}")
+        print(f"Processing class {DICT[i]}, lead {lead+1}")
         try:
-            lead_signal_tensor = torch.from_numpy(lead_signal).float()  # Ensure tensor is float type
+            # Convert numpy array to tensor and make it float
+            lead_signal_tensor = torch.from_numpy(lead_signal).float()
+            # Get the attributions
             attribution = ig.attribute(lead_signal_tensor, target=target_class)
             print("no error in attributions")
             lead_attributions.append(attribution)
-            lead_attr_sums.append(attribution[:,lead,:].sum())
         except Exception as e:
-            print(f"Error in attributions for class {i}, lead {lead}: {e}")
+            print(f"Error in attributions for class {DICT[i]}, lead {lead+1}: {e}")
+        # Initialize plotly figure to write per lead, per class results
         fig = go.Figure()
+        # Create averaging filter
         avg_filter = torch.ones(1, WINDOW_SIZE, dtype=torch.float32) / WINDOW_SIZE
-        avg_filter = avg_filter
         #print(avg_filter.size())
-        # Normalizing lead_attributions and class_signals
+        # Extend attributions
         ecg_attrib = lead_attributions[lead][0,lead,:].unsqueeze(0).detach().numpy()
         #print(ecg_attrib.shape)
-        class_signal = class_signals[i, :, lead, :]
+        # Get lead data for the first ECG in the batch
+        class_signal = class_signals[i, ELEMENT, lead, :]
         #print(class_signal.shape)
-        ecg_temp = ecg_attrib / class_signal[0]
+        # Normalize per ECG amplitude at each timestep
+        ecg_temp = ecg_attrib / class_signal
+        # Make it a tensor, float and add a dimension
         ecg_temp = torch.from_numpy(ecg_temp).view(1, 1, -1).float()
-        
+        # Perform the convolution
         smoothed = F.conv1d(ecg_temp, avg_filter.view(1, 1, -1), padding=WINDOW_SIZE//2)
         #print(smoothed.size())
+        # Flatten the tensor
         smoothed = smoothed.view(-1)
         #print(smoothed.size())
+        # Padd appropriate with the last seen value
         smoothed[:WINDOW_SIZE//2] = smoothed[0].item()
         smoothed[-WINDOW_SIZE//2:] = smoothed[-1].item()
         #print(smoothed.size())
 
-        fig.add_trace(go.Scatter(y=class_signal[lead], name=f"Lead {lead+1} ECG Signal"))    
+        # Add original signal and attributions to the plot
+        fig.add_trace(go.Scatter(y=class_signal, name=f"Lead {lead+1} ECG Signal"))    
         fig.add_trace(go.Scatter(y=smoothed.numpy()/np.max(smoothed.numpy()), name=f"Lead {lead+1} Attributions"))
         fig.update_layout(title=f"Lead {lead+1} ECG Signal and Attributions fo class {DICT[i]}")
         fig.show()
